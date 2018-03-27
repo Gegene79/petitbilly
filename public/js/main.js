d111caa124cccf3d285efe7ab158d5b1e1c217ef
Gegene79 <fabienvignat@yahoo.fr>
2018-03-27 23:46:25 +0200

    nueva version

--- public/js/main.js
+++ public/js/main.js
@@ -38,22 +38,19 @@ $(function() {
         }
     });
 
+    
+      
+
     $('#blueimp-gallery').on('slide', function (event, index, slide) {
         // Gallery slide event handler
         $(this).children('.description').text($('#inner-container-photos a').eq(index).data('description'));
-    });
-
-    // side-bar button click
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#main-container").toggleClass("toggled");
     });
 
     $('#ExteriorIndicator').radialIndicator({
         displayNumber: false,
         barColor: '#87CEEB',
         radius: 70,
-        barWidth: 2,
+        barWidth: 3,
         roundCorner : false,
         percentage: false,
         minValue: -10,
@@ -69,158 +66,117 @@ $(function() {
     enablemonitor();
 });
 
+
+/*
+    Main week graph
+*/
+function initWeekChart(){
+    
+    // mapear x e y hacia las columnas
+    chartWeek.x(function(d) {
+        var b = new Date(d.x).getTime();
+        return b;
+    });
+    //chart.y(function(d) { return d.value; });
+    // formato ejes
+    chartWeek.xAxis
+        //.staggerLabels(true)
+        .tickFormat(function (d) {
+        return d3.time.format('%a %d - %H:%M')(new Date(d));
+    });
+    
+    chartWeek.x2Axis
+        //.staggerLabels(true)
+        .tickFormat(function (d) {
+        return d3.time.format('%a %d - %H:%M')(new Date(d));
+    });
+    chartWeek.margin({top: 50, right: 50, bottom: 50, left: 50})
+    chartWeek.yTickFormat(d3.format(',.1f'));
+    chartWeek.yAxis.axisLabel("ºC");
+    chartWeek.interpolate("basis");
+    chartWeek.useInteractiveGuideline(true);
+
+    return chartWeek;
+};
+
+function updateWeekChart(){
+
+    var now = new Date();
+    var lastweek = new Date();
+    lastweek.setHours(-24*7,0,0,0);
+
+    d3.json(API_BASEURL+"/temperature?sampling=30&ini="+lastweek.toISOString(), function(error, data) {	
+        if (error) return console.log(error);
+
+        //var max = d3.max(data, function(c) { return d3.max(c.values, function(d) { return d.y; }); })+1; 
+        //var min = d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.y; }); })-1;
+        //chartWeek.forceY([min, max]);
+        
+        var weekticks = [];
+        var s = new Date(lastweek);// new Date($('#hfEventStartDate').val() - 0);
+        while(s.valueOf() < now.valueOf()) {
+            weekticks.push(s);
+            s = new Date(s.setDate(
+                s.getDate() + 1
+            ));
+        }
+
+        chartWeek.xAxis.tickValues(weekticks);
+        chartWeek.x2Axis.tickValues(weekticks);
+
+        d3.select('#chart_week svg')
+            .datum(data)
+            .call(chartWeek);
+
+        nv.utils.windowResize(chartWeek.update);
+    });
+
+    timerGraphWeek = setTimeout(updateWeekChart, TIMER_GWEEK);
+    
+};
+
+function updateCurrentVal(){
     /*
-        6 Hours chart
+    $.getJSON( API_BASEURL+"/temperature/Exterior/current", function( data ) {
+        let temp= parseFloat(data[0].value);
+        var radialObj = $('#ExteriorIndicator').data('radialIndicator');
+        //radialObj.option('displayNumber',false);
+        radialObj.animate(temp);
+        //radialObj.option('displayNumber',true);
+    });
     */
+
+
     
-    function init6HoursChart(){
-    
-        // mapear x e y hacia las columnas
-        chartG6H.x(function(d) {
-            var b = new Date(d.x).getTime();
-            return b;
-        });
-        //chart.y(function(d) { return d.value; });
-        
-        // formato ejes
-        chartG6H.xAxis.tickFormat(function (d) {
-            var a = new Date(d);
-            return d3.time.format('%H:%M')(a);
-        });
+    $.getJSON( API_BASEURL+"/temperature/current", function( data ) {
+        $.each( data, function(key,val) {
+            let id = val._id;
+            let temp= parseFloat(val.value);
+            let m = moment(val.timestamp);
+            let ts = "";
+            if(m.isBefore(new Date(),'day')){
+                ts = m.format('ddd DD/MM HH:mm:ss');
+            } else {
+                ts = m.format('HH:mm:ss');
+            };
 
-        chartG6H.yTickFormat(d3.format(',.1f'));
-        chartG6H.yAxis.axisLabel("ºC");
-
-        chartG6H.interpolate("basis");
-        chartG6H.useInteractiveGuideline(true);
-
-        return chartG6H;
-    };
-
-    function updateChart6Hours(){
-
-        var ini = new Date();
-        ini.setHours(ini.getHours()-6,0,0,0);
-
-        d3.json(API_BASEURL+"/temperature?sampling=5&ini="+ini.toISOString(), function(error, data) {
-
-            var max = d3.max(data, function(c) { return d3.max(c.values, function(d) { return d.y; }); })+1; 
-            var min = d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.y; }); })-1;
-            chartG6H.forceY([min, max]);
-
-            d3.select('#chart_6hours svg')
-                    .datum(data)
-                    .call(chartG6H);
-        
-            nv.utils.windowResize(chartG6H.update);
-        });
-        timerGraph2hours = setTimeout(updateChart6Hours, TIMER_G2H);
-    };
-
-    /*
-        Main week graph
-    */
-    function initWeekChart(){
-        
-        // mapear x e y hacia las columnas
-        chartWeek.x(function(d) {
-            var b = new Date(d.x).getTime();
-            return b;
-        });
-        //chart.y(function(d) { return d.value; });
-        // formato ejes
-        chartWeek.xAxis
-            //.staggerLabels(true)
-            .tickFormat(function (d) {
-            return d3.time.format('%a %d - %H:%M')(new Date(d));
-        });
-        
-        chartWeek.x2Axis
-            //.staggerLabels(true)
-            .tickFormat(function (d) {
-            return d3.time.format('%a %d - %H:%M')(new Date(d));
-        });
-        chartWeek.margin({top: 50, right: 50, bottom: 50, left: 50})
-        chartWeek.yTickFormat(d3.format(',.1f'));
-        chartWeek.yAxis.axisLabel("ºC");
-        chartWeek.interpolate("basis");
-        chartWeek.useInteractiveGuideline(true);
-
-        return chartWeek;
-    };
-
-    function updateWeekChart(){
-
-        var now = new Date();
-        var lastweek = new Date();
-        lastweek.setHours(-24*7,0,0,0);
-    
-        d3.json(API_BASEURL+"/temperature?sampling=30&ini="+lastweek.toISOString(), function(error, data) {	
-            if (error) return console.log(error);
-
-            //var max = d3.max(data, function(c) { return d3.max(c.values, function(d) { return d.y; }); })+1; 
-            //var min = d3.min(data, function(c) { return d3.min(c.values, function(d) { return d.y; }); })-1;
-            //chartWeek.forceY([min, max]);
-            
-            var weekticks = [];
-            var s = new Date(lastweek);// new Date($('#hfEventStartDate').val() - 0);
-            while(s.valueOf() < now.valueOf()) {
-                weekticks.push(s);
-                s = new Date(s.setDate(
-                    s.getDate() + 1
-                ));
+            // update radial indicator
+            if (id=="Exterior"){
+                var radialObj = $('#ExteriorIndicator').data('radialIndicator');
+                radialObj.animate(temp);
             }
 
-            chartWeek.xAxis.tickValues(weekticks);
-            chartWeek.x2Axis.tickValues(weekticks);
-
-            d3.select('#chart_week svg')
-                .datum(data)
-                .call(chartWeek);
-
-            nv.utils.windowResize(chartWeek.update);
+            $("#temp-"+id).text(temp.toFixed(1));
+            $("#temp-ts-"+id).text(ts);
+            
         });
-
-        timerGraphWeek = setTimeout(updateWeekChart, TIMER_GWEEK);
-        
-    };
-
-    function updateCurrentVal(){
-
-        $.getJSON( API_BASEURL+"/temperature/Exterior/current", function( data ) {
-            let temp= parseFloat(data[0].value);
-            var radialObj = $('#ExteriorIndicator').data('radialIndicator');
-            //radialObj.option('displayNumber',false);
-            radialObj.animate(temp);
-            //radialObj.option('displayNumber',true);
-        });
+    });
 
 
+    //$.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}")
 
-        
-        $.getJSON( API_BASEURL+"/temperature/current", function( data ) {
-            $.each( data, function(key,val) {
-                let id = val._id;
-                let temp= parseFloat(val.value);
-                let m = moment(val.timestamp);
-                let ts = "";
-                if(m.isBefore(new Date(),'day')){
-                    ts = m.format('ddd DD/MM HH:mm:ss');
-                } else {
-                    ts = m.format('HH:mm:ss');
-                };
-
-                $("#temp-"+id).text(temp.toFixed(1));
-                $("#temp-ts-"+id).text(ts);
-                
-            });
-        });
-
-
-        $.getJSON("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}")
-
-        timerCurrentVal = setTimeout(updateCurrentVal, TIMER_CURRENT);
-    };
+    timerCurrentVal = setTimeout(updateCurrentVal, TIMER_CURRENT);
+};
 
 
 
@@ -230,14 +186,14 @@ function enablephotos(){
     clearTimeout(timerGraph2hours);
     clearTimeout(timerGraphWeek);
 
-    $('#container-monitor').addClass('hidden');
+    $('#container-monitor').addClass('d-none');
     $('.nvtooltip').remove();
 
-    $('#container-photos').removeClass('hidden');
-    $('#div-search-bar').removeClass('hidden');
-    $('#div-text-results').removeClass('hidden');
-    $('#div-nav-pg-btn').removeClass('hidden');
-    $('#scan-btn').removeClass('hidden');
+    $('#container-photos').removeClass('d-none');
+    $('#div-search-bar').removeClass('d-none');
+    $('#div-text-results').removeClass('d-none');
+    $('#div-nav-pg-btn').removeClass('d-none');
+    $('#scan-btn').removeClass('d-none');
 
     $('#monitor').parent().removeClass('active');
     $('#photos').parent().addClass('active');
@@ -266,13 +222,13 @@ function search(query){
 
 
 function enablemonitor(){
-    $('#container-photos').addClass('hidden');
+    $('#container-photos').addClass('d-none');
     $('#div-search-bar').addClass('hidden');
     $('#div-text-results').addClass('hidden');
     $('#div-nav-pg-btn').addClass('hidden');
     $('#scan-btn').addClass('hidden');
     
-    $('#container-monitor').removeClass('hidden');
+    $('#container-monitor').removeClass('d-none');
     $('#monitor').parent().addClass('active');
     $('#photos').parent().removeClass('active');
 
@@ -338,7 +294,7 @@ function scan(){
 }
 
 function populateimages(imgarray,totalcount){
-
+    /*
     $('#inner-container-photos').empty();
 
     var yearmonth = "";
@@ -355,16 +311,97 @@ function populateimages(imgarray,totalcount){
             }
             line++;
             yearmonth = itemyearmonth;
-            $('<div id="line'+line+'" class="row">').appendTo('#inner-container-photos');
-            $('<div class="page-header"><h3>'+itemyearmonth+'</h3></div>').appendTo('#line'+line);
+            $('<div id="line'+line+'" class="row no-gutters">').appendTo('#inner-container-photos');
+            $('<div class="col-12"><h2>'+itemyearmonth+'</h2></div>').appendTo('#line'+line);
         }
         let score = (item.score)? ' - score: '+item.score.toFixed(2) : '';
-        $('<div class="col-xs-6 col-md-4"> <a href="'+item.largethumb+
-        '" class="thumbnail" title="'+item.filename+'" data-description="'+created_at+'" data-gallery> \
-        <img src="'+item.smallthumb+'" />'+created_at+score+'</a></div>').appendTo('#line'+line);
+        $(' <div class="col-12 col-md-6 col-lg-3 col-xl-2"> \
+                    <a href="'+item.largethumb+'" title="'+ 
+                        item.filename+'" data-description="'+created_at+'" data-gallery> \
+                        <img src="'+item.smallthumb+'" class="img-fluid"/></a> \
+                    <div class="card-img-overlay"> \
+                        <div class="icon_overlay"> \
+                        <i class="fas fa-download fa-lg" data-fa-transform="shrink-3.5" data-fa-mask="fas fa-circle"> \
+                    <a href="#" /></i> \
+                    </div> \
+            </div>').appendTo('#line'+line);
 
     });
     $('</div>').appendTo('#line'+line);
+    */
+
+   //$('#inner-container-photos').empty();
+
+   var yearmonth = "";
+   let line = 0;
+   $.each( imgarray, function( i, item ) {
+   
+       let m = moment(item.created_at);
+       m.locale('es');
+       let itemyearmonth = m.format('MMMM YYYY');
+       let created_at = m.format('DD MMM YYYY HH:mm:ss');
+       let camara='';
+        try {
+            camara=item.info.Make + ' '+ item.info.Model;
+        } catch (error) {
+            console.log("error en datos info en fichero "+item.filename);
+        }
+
+       if (itemyearmonth != yearmonth) {
+           if (line!=0){
+               $('</div>').appendTo('#line'+line);
+           }
+           line++;
+           yearmonth = itemyearmonth;
+           $('<div id="line'+line+'" class="row">').appendTo('#inner-container-photos');
+           $('<div class="col-12 m-1"><h6>'+itemyearmonth+'</h6></div>').appendTo('#line'+line);
+       }
+       let score = (item.score)? ' - score: '+item.score.toFixed(2) : '';
+       /*
+       <div class="col-6 col-lg-4">
+            <div class="card bg-light text-white">
+                <a class="icon-overlay position-absolute p-1" href="javascript:alert('downdload');"><i class="fas fa-arrow-alt-circle-down fa-lg"></i></a>
+                <a class="icon-overlay position-absolute p-1" style="left:1.5rem; "href="javascript:alert('info');" role="button" data-toggle="popover" data-trigger="focus" data-placement="right" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?"><i class="fas fa-info-circle fa-lg"></i></a>
+                
+                <a class="card-link" href="javascript:alert('image');">
+                    <img class="card-img" src="http://ultraimg.com/images/photo-1.jpg" />
+                    <div class="card-img-overlay fondo"></div>
+                </a>
+        
+            </div>
+        </div>
+       */
+      $( '<div class="col-12 col-sm-6 col-lg-4"> \
+            <div class="card bg-light text-white mx-auto"> \
+                <a class="icon-overlay position-absolute p-1" href="'+item.path+'"><i class="fas fa-arrow-alt-circle-down fa-lg"></i></a> \
+                <a class="icon-overlay position-absolute p-1" style="left:1.5rem;" href="javascript:event.preventDefault();" role="button" data-toggle="popover" data-trigger="focus" data-placement="right"  \
+                title="' + item.filename + '" data-html="true" data-content="<ul><li>Dir: '+item.dir+'</li><li>Fecha: '+ created_at +'</li><li>Camara: ' + camara +'</li></ul>"> \
+                <i class="fas fa-info-circle fa-lg"></i></a> \
+                <a class="card-link" href="#"> \
+                    <img class="card-img" src="'+item.smallthumb+'" /> \
+                    <div class="card-img-overlay fondo"></div> \
+                </a> \
+            </div> \
+        </div>').appendTo('#line'+line);
+
+       /*
+       $(   '<div class="col-6 col-xl-4"><a href="'+item.largethumb+'" title="'+ 
+                       item.filename+'" data-description="'+created_at+'" data-gallery> \
+                       <img src="'+item.smallthumb+'" class="img-fluid"/> \
+                       </a> \
+                <div class="icon_overlay"> \
+                    <i class="fas fa-download fa-lg" data-fa-transform="shrink-3.5" data-fa-mask="fas fa-circle"> \
+                    <a href="#" /></i> \
+                </div> \
+            </div>').appendTo('#line'+line);
+        */
+   });
+   $('</div>').appendTo('#line'+line);
+
+   // initialize photos' popovers
+   $('[data-toggle="popover"]').popover({
+    container: 'body'
+  });
 };
 
 function updatenav(pgnumber, currentpg){
